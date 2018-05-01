62fd6e25ee2e5a07495ed912365d4ecb9d1fbcde
marcotuna <marcotunas@gmail.com>
2018-05-01 23:58:34 +0100

    Update app.go

--- app.go
+++ app.go
@@ -47,6 +47,7 @@ func DoDNSSet(w http.ResponseWriter, r *http.Request) {
 	dnsCmdDeleteRecord := exec.Command("cmd", "/C", "dnscmd /recorddelete "+zoneName+" "+nodeName+" "+dnsType+" /f")
 
 	if err := dnsCmdDeleteRecord.Run(); err != nil {
+		//respondWithError(w, http.StatusBadRequest, "Could not run dnscmd /recorddelete")
 		respondWithError(w, http.StatusBadRequest, "Could not run dnscmd /recorddelete")
 		return
 	}
@@ -63,6 +64,44 @@ func DoDNSSet(w http.ResponseWriter, r *http.Request) {
 
 // DoDNSRemove Remove
 func DoDNSRemove(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	zoneName, dnsType, nodeName, ipAddress := vars["zoneName"], vars["dnsType"], vars["nodeName"], vars["ipAddress"]
+
+	// Validate DNS Type
+	if dnsType != "A" {
+		respondWithError(w, http.StatusBadRequest, "You specified an invalid record type ('"+dnsType+"'). Currently, only the 'A' (alias) record type is supported.  e.g. /dns/my.zone/A/..")
+	}
+
+	// Validate DNS Type
+	var validZoneName = regexp.MustCompile(`[^A-Za-z0-9\.-]+`)
+
+	if validZoneName.MatchString(zoneName) {
+		respondWithError(w, http.StatusBadRequest, "Invalid zone name ('"+zoneName+"'). Zone names can only contain letters, numbers, dashes (-), and dots (.).")
+		return
+	}
+
+	// Validate Node Name
+	var validNodeName = regexp.MustCompile(`[^A-Za-z0-9\.-]+`)
+
+	if validNodeName.MatchString(nodeName) {
+		respondWithError(w, http.StatusBadRequest, "Invalid node name ('"+nodeName+"'). Node names can only contain letters, numbers, dashes (-), and dots (.).")
+		return
+	}
+
+	// Validate Ip Address
+	var validIPAddress = regexp.MustCompile(`^(([1-9]?\d|1\d\d|25[0-5]|2[0-4]\d)\.){3}([1-9]?\d|1\d\d|25[0-5]|2[0-4]\d)$`)
+
+	if !validIPAddress.MatchString(ipAddress) {
+		respondWithError(w, http.StatusBadRequest, "Invalid IP address ('"+ipAddress+"'). Currently, only IPv4 addresses are accepted.")
+		return
+	}
+
+	dnsCmdDeleteRecord := exec.Command("cmd", "/C", "dnscmd /recorddelete "+zoneName+" "+nodeName+" "+dnsType+" /f")
+
+	if err := dnsCmdDeleteRecord.Run(); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Could not run dnscmd /recorddelete")
+		return
+	}
 
 }
 
